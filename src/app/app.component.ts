c7e1fa9232d41bec7fa1c2ee115bd9f56b3f6661
William Brito <williamrldb@gmail.com>
2023-04-11 18:36:24 -0400

    --Curso Angular 13: Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'William';
+  userData = {
+    email: 'williamrldb@gmail.com',
+    role: 'admin',
+  };
   title = 'curso-angular-yt';
 }

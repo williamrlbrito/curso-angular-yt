e478b8c4f5205f174f6637acf3fc96cdd8d38a93
William Brito <williamrldb@gmail.com>
2023-04-10 20:16:30 -0400

    --Curso Angular 13: Dados no template

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css'],
 })
-export class FirstComponentComponent {}
+export class FirstComponentComponent {
+  name: string = 'William Brito';
+  age: number = 35;
+  job = 'Programmer';
+  hobbies = ['run', 'play', 'study'];
+  car = {
+    name: 'Polo',
+    year: 2019,
+  };
+}
